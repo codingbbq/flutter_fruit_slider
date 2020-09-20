7e860a4011138156df92029081c6eb5d36a31764
Codding BBQ <codingbbq@gmail.com>
2020-09-20 11:07:35 +0530

    Adding controller to the app

--- lib/pages/fruits/apple_card.dart
+++ lib/pages/fruits/apple_card.dart
@@ -4,34 +4,30 @@ class AppleCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Center(
-            child: Container(
-              width: 250,
-              height: 250,
-              color: Colors.red[200],
-            ),
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Center(
+          child: Container(
+            width: 250,
+            height: 250,
+            color: Colors.red[600],
           ),
-          SizedBox(
-            height: 30.0,
-          ),
-          Text(
-            "Apple",
-            style: TextStyle(
-              fontSize: 30.0
-            ),
-          ),
-          SizedBox(
-            height: 30.0,
-          ),
-          Text(
-            "An apple is an edible fruit produced by an apple tree (Malus domestica). Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia.",
-            style: TextStyle(
-              fontSize: 16.0
-            ),
-          )
-        ],
-      );
+        ),
+        SizedBox(
+          height: 30.0,
+        ),
+        Text(
+          "Apple",
+          style: TextStyle(fontSize: 30.0),
+        ),
+        SizedBox(
+          height: 30.0,
+        ),
+        Text(
+          "An apple is an edible fruit produced by an apple tree (Malus domestica). Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia.",
+          style: TextStyle(fontSize: 16.0),
+        )
+      ],
+    );
   }
 }
--- lib/pages/fruits/fruits_page.dart
+++ lib/pages/fruits/fruits_page.dart
@@ -8,8 +8,9 @@ class FruitsPage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: <Widget>[
-            Header(),
-            Body(),
+            Header(), // The back button
+            Body(), // PageView Widget
+            Navigation() // Controlling the pageView widget
           ],
         ),
       ),
@@ -24,7 +25,7 @@ class Header extends StatelessWidget {
       child: Row(
         children: <Widget>[
           IconButton(
-            onPressed: (){
+            onPressed: () {
               Navigator.pop(context);
             },
             icon: Icon(Icons.arrow_back_ios),
@@ -35,21 +36,52 @@ class Header extends StatelessWidget {
   }
 }
 
+PageController controller = PageController();
+
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height - 80,
-      color: Colors.yellow,
-      padding: EdgeInsets.symmetric(vertical: 30.0, horizontal: 30.0),
-      child: PageView(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height - 140,
+        color: Colors.yellow,
+        padding: EdgeInsets.symmetric(vertical: 30.0, horizontal: 30.0),
+        child: PageView(
+          controller: controller,
           scrollDirection: Axis.horizontal,
-          children: <Widget>[
-            AppleCard(),
-            AppleCard()
-          ],
-        )
+          children: <Widget>[AppleCard(), AppleCard()],
+        ));
+  }
+}
+
+class Navigation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      width: 150,
+      color: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.brightness_1),
+            onPressed: () {
+              controller.jumpToPage(2);
+            },
+            iconSize: 20,
+            color: Colors.red,
+          ),
+          IconButton(
+            icon: Icon(Icons.brightness_1),
+            onPressed: () {
+              controller.jumpToPage(1);
+            },
+            iconSize: 20,
+            color: Colors.red,
+          )
+        ],
+      ),
     );
   }
 }
